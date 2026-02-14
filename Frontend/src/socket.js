7f5d5e5f26dc37e89386b487cdcbdf1d9330a415
Abhishek Kumar Pundir <abhishekgigaiw@gmail.com>
2026-02-14 06:36:43 +0530

    happend

--- Frontend/src/socket.js
+++ Frontend/src/socket.js
@@ -4,10 +4,16 @@ let socket;
 
 export const getSocket = () => {
   if (!socket) {
-    socket = io("http://localhost:4000", {
+    const backendURL =
+      window.location.hostname === "localhost"
+        ? "http://localhost:4000"
+        : "https://helpx-vnvg.onrender.com";
+
+    socket = io(backendURL, {
       withCredentials: true,
       transports: ["websocket"]
     });
   }
+
   return socket;
 };
